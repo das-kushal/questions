5cb2f8f6151776370181fb67a265f893a43baa9f
Kushal Das <kushaldas30102002@gmail.com>
2025-08-23 23:49:02 +0530

    ---

--- header.h
+++ header.h
@@ -155,14 +155,6 @@ struct TreeNode
 {
     int val;
     TreeNode *left, *right;
-
-    TreeNode(int data)
-    {
-        val = data;
-        left = nullptr;
-        right = nullptr;
-    }
-
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
