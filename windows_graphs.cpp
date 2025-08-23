7ab35615367aa6b2cbeb1b0118374e37951656e9
Kushal Das <kushaldas30102002@gmail.com>
2025-08-23 23:31:26 +0530

    graph rotten tomato

--- windows_graphs.cpp
+++ windows_graphs.cpp
@@ -2,6 +2,145 @@
 
 using namespace std;
 
+class BipartiteDFS
+{
+private:
+    bool canBeColored(int node, vector<vector<int>> &graph, vector<int> &color, int col)
+    {
+        color[node] = col;
+
+        for (auto it : graph[node])
+        {
+            if (color[it] == -1)
+            {
+                if (!canBeColored(it, graph, color, !col))
+                    return false;
+            }
+            else if (color[it] == color[node])
+                return false;
+        }
+
+        return true;
+    }
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int n = graph.size();
+        vector<int> color(n, -1); // uncolored
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (color[i] == -1)
+            {
+                if (!canBeColored(i, graph, color, 1))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+class BipartiteBFS
+{
+private:
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int n = graph.size();
+        vector<int> colors(n, -1);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (colors[i] == -1)
+            {
+                queue<int> q;
+                q.push(i);
+
+                while (!q.empty())
+                {
+                    int node = q.front();
+                    q.pop();
+
+                    for (auto it : graph[node])
+                    {
+                        if (colors[it] == -1)
+                        {
+                            colors[it] = !colors[node];
+                            q.push(it);
+                        }
+                        else if (colors[it] == colors[node])
+                            return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+class RottingOranges
+{
+private:
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<vector<int>> q; // row col time
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (grid[i][j] == 2)
+                {
+                    q.push({i, j, 0});
+                    grid[i][j] = 3;
+                }
+            }
+        }
+        int dr[4] = {1, -1, 0, 0};
+        int dc[4] = {0, 0, 1, -1};
+        int maxTime = 0;
+        while (!q.empty())
+        {
+            int len = q.size();
+            for (int i = 0; i < len; ++i)
+            {
+                auto it = q.front();
+                int row = it[0];
+                int col = it[1];
+                int curTime = it[2];
+                maxTime = max(maxTime, curTime);
+                q.pop();
+
+                for (int k = 0; k < 4; ++k)
+                {
+                    int nR = row + dr[k];
+                    int nC = col + dc[k];
+
+                    if (nR >= 0 && nC >= 0 && nR < n && nC < m && grid[nR][nC] == 1)
+                    {
+                        q.push({nR, nC, curTime + 1});
+                        grid[nR][nC] = 3;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (grid[i][j] == 1)
+                {
+                    return -1;
+                }
+            }
+        }
+
+        return maxTime;
+    }
+};
+
 int main()
 {
     return 0;
