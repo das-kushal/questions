f8e6e3d8ec277ee106b5e062b9248b94604157e6
Kushal Das <kushaldas30102002@gmail.com>
2025-08-24 19:57:59 +0530

    ====

--- windows_graphs.cpp
+++ windows_graphs.cpp
@@ -315,6 +315,54 @@ private:
 
         return count == numCourses;
     }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        vector<int> order;
+        queue<int> q;
+        vector<vector<int>> adj(numCourses);
+
+        for (auto it : prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; ++i)
+        {
+            for (auto it : adj[i])
+                indegree[it]++;
+        }
+
+        for (int i = 0; i < indegree.size(); ++i)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        return order.size() == numCourses ? order : vector<int>();
+    }
+};
+
+class EventualSafeStates
+{
+private:
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    {
+        return {};
+    }
 };
 
 int main()
