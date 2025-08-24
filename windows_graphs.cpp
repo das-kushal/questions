6a62416ab6e53eed3bfbe74836191596b3c193ce
Kushal Das <kushaldas30102002@gmail.com>
2025-08-24 11:02:39 +0530

    =OOO

--- windows_graphs.cpp
+++ windows_graphs.cpp
@@ -268,6 +268,55 @@ private:
     }
 };
 
+class CourseSchedule
+{
+private:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        // find loop in directed graph
+        // if loop then not possible to complete
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for (auto it : prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; ++i)
+        {
+            for (auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        for (int i = 0; i < indegree.size(); i++)
+            if (indegree[i] == 0)
+                q.push(i);
+
+        int count = 0;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            count++;
+
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        return count == numCourses;
+    }
+};
+
 int main()
 {
     return 0;
